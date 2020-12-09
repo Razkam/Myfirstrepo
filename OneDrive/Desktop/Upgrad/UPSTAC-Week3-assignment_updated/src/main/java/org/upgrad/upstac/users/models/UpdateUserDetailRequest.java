5e0a5aae68685749b1b7d57f8bc3d9503bdc10f5
Razkam <57940257+Razkam@users.noreply.github.com>
2020-12-09 15:07:18 +0000

    first project

---
+++ OneDrive/Desktop/Upgrad/UPSTAC-Week3-assignment_updated/src/main/java/org/upgrad/upstac/users/models/UpdateUserDetailRequest.java
@@ -0,0 +1,37 @@
+package org.upgrad.upstac.users.models;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UpdateUserDetailRequest {
+
+    @ApiModelProperty(example = "MK")
+    private String firstName;
+
+
+    @ApiModelProperty(example = "Gandhi")
+    private String lastName;
+
+    @ApiModelProperty(example = "newuser@upgrad.com")
+    private String email="";
+
+    @ApiModelProperty(example = "+91956567687")
+    private String phoneNumber="";
+
+    public String getPhoneNumber() { return phoneNumber;
+    }
+
+    public String getEmail() { return email;
+    }
+
+    public String getFirstName() { return firstName;
+    }
+
+    public String getLastName() { return  lastName;
+    }
+}
